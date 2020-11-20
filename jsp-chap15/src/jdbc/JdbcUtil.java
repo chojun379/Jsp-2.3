e495734b075fff9f1d5b692b06341f2f97b79982
chojun379 <chojun379@gmail.com>
2020-11-20 09:53:12 +0900

    jsp-2.3 chap15

--- jsp-chap15/src/jdbc/JdbcUtil.java
+++ jsp-chap15/src/jdbc/JdbcUtil.java
@@ -30,7 +30,7 @@ public class JdbcUtil {
 	public static void close(Connection conn) {
 		if(conn != null) {
 			try {
-				conn.rollback();
+				conn.close();
 			} catch(SQLException ex) {
 				
 			}
@@ -40,7 +40,7 @@ public class JdbcUtil {
 	public static void rollback(Connection conn) {
 		if(conn != null) {
 			try {
-				conn.close();
+				conn.rollback();
 			} catch(SQLException ex) {
 				
 			}
